6b182b25b8cb45226c3ea6a94cebe7af33694e69
Bakhary Drummond <bdrummond3625@bths.edu>
2018-06-14 11:28:17 -0400

    Created skeletal classes

--- Capstone/Classes/FileReader.cs
+++ Capstone/Classes/FileReader.cs
@@ -11,7 +11,7 @@ namespace Capstone.Classes
 
         public string Path { get; }
 
-
+        public Dictionary<string,>
 
 
 
--- Capstone/Classes/VMItem.cs
+++ Capstone/Classes/VMItem.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Capstone.Classes
 {
-    public class VendingMachineItem
+    public class VMItem
     {
     }
 }
